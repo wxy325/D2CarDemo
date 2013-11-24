9de092b8a6ae9c56cb28f1154d980d63e85cfc5a
wxy325 <wxy325@qq.com>
2013-11-25 00:07:58 +0800

    Initial Commit

---
+++ D2Demo/WXYViewController.h
@@ -0,0 +1,13 @@
+//
+//  WXYViewController.h
+//  D2Demo
+//
+//  Created by wxy325 on 11/25/13.
+//  Copyright (c) 2013 Tongji Univ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXYViewController : UIViewController
+
+@end
---
+++ D2Demo/WXYViewController.m
@@ -0,0 +1,29 @@
+//
+//  WXYViewController.m
+//  D2Demo
+//
+//  Created by wxy325 on 11/25/13.
+//  Copyright (c) 2013 Tongji Univ. All rights reserved.
+//
+
+#import "WXYViewController.h"
+
+@interface WXYViewController ()
+
+@end
+
+@implementation WXYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
